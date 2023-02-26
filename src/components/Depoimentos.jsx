f68b27e6e6c664953f802eb5d60bcf44f3b7c57c
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-25 21:36:48 -0300

    Adiciona imagens e descrição ao "depoimentos"

--- src/components/Depoimentos.jsx
+++ src/components/Depoimentos.jsx
@@ -7,10 +7,10 @@ function Depoimentos() {
         <h2 className='mt-3 pt-4 pb-2'>Depoimentos</h2>
         <Row className='mt-3'>
             <Col sm="12" md="2" className='text-center'>
-                <img src="https://via.placeholder.com/100/ffd000" alt="Foto do depoimento" className='rounded' />
+                <img src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80" alt="Foto do depoimento" className='rounded' />
             </Col>
             <Col sm="12" md="10">
-                <strong>Ana Clara Lima</strong>
+                <strong>Maria Eduarda</strong>
                 <p>
                     "Simplesmente amo a DevPlayTV!<br />
                     Eu sempre me perdia em qual episódio havia parado nas séries, mas com o DevPlayTV posso anotar quais séries já assisti, quais estou assistindo e qual último episódio assistido. Me salva muito!!"
@@ -19,13 +19,12 @@ function Depoimentos() {
         </Row>
         <Row className='mt-3'>
             <Col sm="12" md="2" className='text-center'>
-                <img src="https://via.placeholder.com/100/ffd000" alt="Foto do depoimento 2" className='rounded' />
+                <img src="https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=766&q=80" alt="Foto do depoimento 2" className='rounded' />
             </Col>
             <Col sm="12" md="10">
-                <strong>Depoimento 2</strong>
+                <strong>Mateus Borges</strong>
                 <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus at voluptate voluptatibus illo ad vel ut numquam, quas sunt hic esse, dolore recusandae est vero quod minima eius repellat."
+                "Muito intuitivo!<br /> Além de fácil de usar, as listas ficam muito bem organizadas."
                 </p>
             </Col>
         </Row>        
