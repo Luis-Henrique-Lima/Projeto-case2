ad0020a8a3b4e88107a82085195b9a705c93458a
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-25 21:36:15 -0300

    Adiciona imagens e descrições ao "destaques"

--- src/components/Destaques.jsx
+++ src/components/Destaques.jsx
@@ -1,12 +1,13 @@
 import {Row, Col, Card, Button} from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import '../assets/css/main.css'
 
 function Destaques() {
     return (
         <Row className='mt-2'>
             <Col className='mt-2' sm="12" md="4">
                 <Card>
-                    <Card.Img variant="top" src="https://via.placeholder.com/300" />
+                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" className ="rounded" />
                     <Card.Body>
                         <Card.Title>Fácil de usar</Card.Title>
                         <Card.Text>
@@ -20,7 +21,7 @@ function Destaques() {
             </Col>
             <Col className='mt-2' sm="12" md="4">
                 <Card>
-                    <Card.Img variant="top" src="https://via.placeholder.com/300" />
+                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1603899122634-f086ca5f5ddd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" />
                     <Card.Body>
                         <Card.Title>Rápido e confiável</Card.Title>
                         <Card.Text>
