c8b4f031081f5a329e382b5358d8b4536415765f
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-27 14:22:54 -0300

    inserção de imagem e botão

--- src/components/ListaFuncionalidades.jsx
+++ src/components/ListaFuncionalidades.jsx
@@ -1,3 +1,4 @@
+import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -6,19 +7,21 @@ function ItemFuncionalidade(item) {
     return (
         <Col sm="12" md="4" key={item.id}>
             <Card className='m-3'>
+            <Card.Img variant="top" src="./img1.png"/>
                 <Card.Body>
                     <Card.Title>
                         <strong>{item.nome}</strong>
                     </Card.Title>
-                    <Card.Text>
+                        <Card.Text style={{marginBottom:"0"}}>
                         {item.genero}
                     </Card.Text>
-                    <Card.Text>
-                        {item.ano}
-                    </Card.Text>
-                    <Card.Text>
+                    <Card.Text style={{marginBottom:"0"}}>
                         {item.duracao}
                     </Card.Text>
+                    <Card.Text style={{marginBottom:"0"}}>
+                        {item.ano}
+                    </Card.Text>
+                    <Button style={{width:"100%"}} variant="success">ASSISTIR FILME</Button>
                 </Card.Body>
             </Card>
         </Col>
