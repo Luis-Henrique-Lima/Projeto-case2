e379aaf865f0984cfb5563b05a182513350b62ed
Luan Martins <112735349+luanmartins8@users.noreply.github.com>
2023-02-26 21:55:02 -0300

    Adiciona imagens ao carousel

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -6,40 +6,29 @@ function Banners() {
                 <Carousel.Item>
                 <img
                         className="d-block w-100"
-                        src="https://source.unsplash.com/random/1500x300/?cinema
-                        .catch(error => console.error(error)) "
+                        src="../banner-img1.png"
                         alt="Último slide"
                     />
-                    <Carousel.Caption>
-                        <h5>EstudoApp</h5>
-                        <p>Um app para facilitar a vida do estudante!</p>
+                    <Carousel.Caption>   
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                 <img
                         className="d-block w-100"
-                        src="https://source.unsplash.com/random/1500x300/?cinema
-                        .catch(error => console.error(error)) "
+                        src="../banner-img2.png"
                         alt="Último slide"
                     />
                 
-                    <Carousel.Caption>
-                        <h5>Quais são as funcionalidades?</h5>
-                        <p>Confira os principais recursos disponíveis no app</p>
+                    <Carousel.Caption>  
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="https://source.unsplash.com/random/1500x300/?cinema
-                        .catch(error => console.error(error)) "
+                        src="../banner-img3.png"
                         alt="Último slide"
                     />
                     <Carousel.Caption>
-                        <h5>Contato para parcerias?</h5>
-                        <p>
-                            Entre em contato com a nossa equipe para conversar sobre oportunidades de parceria
-                        </p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
