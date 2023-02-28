6be7b1e113e806d0e1d2cc6fa9a94e1efe30a3e7
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-27 21:39:18 -0300

    Inserção de privatização da rota

---
+++ front/src/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+import { Route, Navigate } from "react-router-dom";
+
+export default function PrivateRoute({ children }) {
+    const isAuthenticated = localStorage.getItem('token');
+        // localStorage.clear()
+    console.log(isAuthenticated);
+
+    return isAuthenticated ? children : <Navigate to="/login" replace />;
+}
