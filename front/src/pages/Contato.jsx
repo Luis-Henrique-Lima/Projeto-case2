7657263ac911193c556bf1e2a164de19d88f6894
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-28 15:41:58 -0300

    inserção do post

--- front/src/pages/Contato.jsx
+++ front/src/pages/Contato.jsx
@@ -1,29 +1,59 @@
 import {Container, Button, Form} from 'react-bootstrap'
+import {useEffect, useState} from 'react'
 import Depoimentos from '../components/Depoimentos'
-import "../css/Contato.css"
-function Contato() {
+import CmsApi from '../api/CmsApi'
+import "../assets/css/Contato.css"
 
-    const handleSubmit = (event) => {
+function Contato() {
+    const [contatos, setContatos] = useState([])
+
+    useEffect (() => {
+        async function getContatos() {
+            const response = await CmsApi().getContatos()
+            const contatos = await response.json()
+            setContatos(contatos.data)
+        }
+
+        getContatos()
+    }, [])
+
+
+    async function handleSubmit(event) {
         event.preventDefault()
-        alert('Mensagem enviada com sucesso!')
-        event.target.reset()
+        const form = event.currentTarget
+        const nome = form.nome.value
+        const email = form.email.value
+        const mensagem = form.mensagem.value
+
+
+        const response = await CmsApi().postContatos({nome, email, mensagem})
+        if(!response.ok) {
+            alert('Erro ao cadastrar mensagem')
+            return
+        }
+        const contato = await response.json()
+        alert('Mensagem cadastrada com sucesso')
+        setContatos([...contatos, contato.data])
+        form.reset()
     }
 
+    
+    
     return (
         <Container className='conteudo-margin pt-5'>
             <h1>Contato</h1>
             <p>Entre em contato conosco</p>
 
             <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="Nome">
+                <Form.Group className="mb-3" controlId="nome">
                     <Form.Label>Nome</Form.Label>
                     <Form.Control className="Form-Control" type="text" placeholder="Digite seu nome completo" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="Email">
+                <Form.Group className="mb-3" controlId="email">
                     <Form.Label>E-mail</Form.Label>
                     <Form.Control className="Form-Control" type="email" placeholder="Digite seu e-mail" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="Mensagem">
+                <Form.Group className="mb-3" controlId="mensagem">
                     <Form.Label>Mensagem</Form.Label>
                     <Form.Control className="Form-Control" as="textarea" rows={3} />
                 </Form.Group>
